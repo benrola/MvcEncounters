3fbafb9a19ddd62ea1d8c321e34e1c906c7f90b6
benrola <reubenster@hotmail.com>
2014-02-01 13:12:24 -0500

    changed to IDnumber and added location property

--- MvcBasics/Models/EBSUser.cs
+++ MvcBasics/Models/EBSUser.cs
@@ -8,6 +8,8 @@ namespace MvcBasics.Models
     public class EBSUser
     {
         public static string UserName { get; set; }
-        public static int ClockNumber { get; set; }
+        public static int IDNumber { get; set; }
+        public static string Location { get; set; }
+
     }
 }
